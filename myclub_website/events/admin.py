707aae23e1c4b3449eb72c7d1760549a2ef56f7f
tharnob <tharnob@gmail.com>
2024-01-21 02:34:55 +0600

    Functionality of Admin Panel

--- myclub_website/events/admin.py
+++ myclub_website/events/admin.py
@@ -5,7 +5,24 @@ from .models import Event
 
 # Register your models here.
 
-admin.site.register(Venue)
+#admin.site.register(Venue)
 admin.site.register(MyClubUser)
-admin.site.register(Event)
+#admin.site.register(Event)
 
+
+# @admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+admin.site.register(Venue, VenueAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date', 'venue')
+    ordering = ('event_date',)
+
+admin.site.register(Event, EventAdmin)
--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,9 +7,9 @@ class Venue(models.Model):
     name = models.CharField(max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField(max_length=15)
-    phone = models.CharField(max_length=25)
-    web = models.URLField()
-    email_address = models.EmailField()
+    phone = models.CharField(max_length=25, blank=True)
+    web = models.URLField(blank=True)
+    email_address = models.EmailField(blank=True)
 
     def __str__(self):
         return self.name
@@ -29,7 +30,7 @@ class Event(models.Model):
     event_date = models.DateTimeField()
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
     #venue = models.CharField(max_lenght=120)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
