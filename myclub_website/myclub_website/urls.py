dc2e5b7df8c33520c4e4607fb261bb7ea4fc3a21
tharnob <tharnob@gmail.com>
2024-01-26 06:22:33 +0600

    Customize Admin Django Title

--- myclub_website/myclub_website/urls.py
+++ myclub_website/myclub_website/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
+
+# Configure Admin Titles
+
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome To The Admin Area"
