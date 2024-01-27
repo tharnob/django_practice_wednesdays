d0466713067dc618a79d7f038db18564e1c47d77
tharnob <tharnob@gmail.com>
2024-01-27 12:49:45 +0600

    Authentication System

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!"))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+
+
+    context = {
+        "form" : form,
+    }
+    return render(request, 'authenticate/register_user.html', context)
+
+
 
 def login_user(request):
     if request.method == "POST":
