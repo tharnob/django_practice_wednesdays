f0dbfff244089ffe8dcba386ca29debf46dcbb7e
tharnob <tharnob@gmail.com>
2024-01-30 12:29:01 +0600

    Remove Groups From Admin Site

--- myclub_website/events/admin.py
+++ myclub_website/events/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import Venue
 from .models import MyClubUser
 from .models import Event
-
+from django.contrib.auth.models import Group
 # Register your models here.
 
 #admin.site.register(Venue)
 admin.site.register(MyClubUser)
 #admin.site.register(Event)
 
+# Remove Groups
+admin.site.unregister(Group)
 
 # @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
